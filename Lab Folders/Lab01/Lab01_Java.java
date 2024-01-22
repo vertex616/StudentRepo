5bd47acbcf939e1368483616db020a55756779da
Yousaf bazaz <yousafbazaz@gmail.com>
2024-01-22 20:35:44 +0000

    Edited the Lab 1

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,7 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
  // Edit made by Yousaf Ahmed Bazaz 220000253.
-
+ // edited by a student
   //Last line print('Good bye')
 }
 
