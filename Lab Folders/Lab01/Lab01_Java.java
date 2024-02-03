981f0b89fb0cd8e4c23337ba50ce7f2421ecfa9b
Yousaf <125975474+vertex616@users.noreply.github.com>
2024-02-03 16:37:01 +0000

    Update Lab01_Java.java

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,7 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
  // Edit made by Yousaf Ahmed Bazaz 220000253.
- // edited by a student
+ // edited by a student and an amazing person
   //Last line print('Good bye')
 }
 
